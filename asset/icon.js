38f4096131218ff9259ee4032c1efe69b5f0ebe6
hwh <1079255145@qq.com>
2020-06-18 19:21:11 +0800

    路径插件,素材, 目录调整

---
+++ asset/icon.js
@@ -0,0 +1,11 @@
+export default {
+    'community': require('../asset/mine/community.png'),
+    'customization': require('../asset/mine/customization.png'),
+    'familySafety': require('../asset/mine/familySafety.png'),
+    'grade': require('../asset/mine/grade.png'),
+    'helpcenter': require('../asset/mine/helpcenter.png'),
+    'mystore': require('../asset/mine/mystore.png'),
+    'otherdevice': require('../asset/mine/otherdevice.png'),
+    'servicecenter': require('../asset/mine/servicecenter.png'),
+    'setting' : require('../asset/mine/setting.png'),
+}
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: ['babel-plugin-root-import'],
+  plugins: [
+   [
+     'babel-plugin-root-import',
+     {
+        root: __dirname,
+        rootPathPrefix: '#/',
+        rootPathSuffix: './',
+    },
+   ]
+ ]
 };
