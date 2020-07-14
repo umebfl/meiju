a67347efc0e56ee18d95eaf172930afba1d4eb9a
hwh <1079255145@qq.com>
2020-07-15 00:47:18 +0800

    add:食谱界面

--- src/discover/menus/index.js
+++ src/discover/menus/index.js
@@ -1,27 +1,121 @@
 import React from 'react'
 
+import * as R from 'ramda'
+
 import {
     View,
     StyleSheet,
     Text,
+    TextInput,
+    SectionList,
+    Keyboard,
 } from 'react-native'
 
+import {
+    WhiteSpace,
+    WingBlank,
+} from '@ant-design/react-native'
+
+import Icon_evilIcons from 'react-native-vector-icons/EvilIcons'
+import Icon_feather from 'react-native-vector-icons/Feather'
+
+import ListData from './listData'
+
+const SearchBar = payload => {
+    const [value, onChangeText] = React.useState('');
+    return (
+        <View style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+             marginLeft: 15,
+             marginRight: 40,
+         }}>
+            <View style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginRight: 20,
+                backgroundColor: '#F6F5F6',
+                borderRadius: 5
+            }}>
+                <Icon_evilIcons style={{marginLeft: 4}} name='search' color='black' size={20}/>
+                <TextInput
+                    style={{ flex: 1,height: 40, borderColor: 'red' }}
+                    onChangeText={text => onChangeText(text)}
+                    maxLength={20}
+                    placeholder={'搜索'}
+                    value={value}
+                />
+                <Icon_evilIcons style={{marginRight: 4}} name='camera' color='black' size={25}/>
+            </View>
+            <Icon_feather name='user' color='black' size={20}/>
+        </View>
+    )
+}
+
+const FoodMenuList = payload => {
+    return (
+        <View>
+            <ImageBackground
+                source={{uri: payload.item.imageURL}}
+                defaultSource={DefaultsImg}
+                style={{
+                    flex:1,
+                    overflow: 'hidden',
+                    borderRadius: 8,
+                    marginHorizontal: 4,
+                    height: 200,
+                    flexDirection: 'column-reverse',}}
+            />
+        </View>
+    )
+}
+
+const Menus = payload => {
+    console.log('----daat>',payload.section.data)
+
+    return (
+        <View style={{flexDirection: 'row'}}>
+            {
+                R.addIndex(R.map)(
+                    (v, k) => (
+                        <View style={{backgroundColor:'#F6F5F6' }}>
+                            <Text key={k}> {v.title} </Text>
+                        </View>
+                    )
+                )(payload.section.data)
+            }
+        </View>
+    )
+}
 
 export default class Connect extends React.Component {
     render() {
+        // console.log('-->',ListData.data)
         return (
             <View style={styles.view}>
-                <Text> menus </Text>
+                <SectionList
+                    sections={ListData.data}
+                    ListHeaderComponent={() =>
+                        <SearchBar/>
+                    }
+                    renderItem={({ item, index, section }) =>
+                        <Text> ss </Text>
+                    }
+                    renderSectionHeader={({section}) =>
+                        <Menus section={section}/>
+                    }
+                    keyExtractor={(item, index) => item + index}
+                    showsVerticalScrollIndicator={false}
+                />
             </View>
+
         )
     }
 }
 
 const styles = StyleSheet.create({
     view: {
-        flex:1,
+        flex: 1,
         backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center'
     }
 })
---
+++ src/discover/menus/listData.js
@@ -0,0 +1,154 @@
+data = [
+    {
+        data:
+        [
+            {
+                title: '今日推荐',
+                foodMenu:
+                [
+                    {
+                        title: '奶香餐包',
+                        tool: '小烤箱',
+                        imgURL: ''
+                    },
+                    {
+                        title: '豆豉尖椒苦瓜',
+                        tool: '小烤箱',
+                        imgURL: ''
+                    },
+                    {
+                        title: '蚝油白菜包',
+                        tool: '小烤箱',
+                        imgURL: ''
+                    },
+                ],
+                foodCollection:
+                [
+                    {
+                        specialTitle: '黑色食物集',
+                        data: [
+                            {
+                                title: '紫菜干蒸烧卖',
+                                time: '5分钟',
+                                difficulty: '困难',
+                                calorie: '400千卡',
+                            },
+                            {
+                                title: '奥利奥甜甜圈',
+                                time: '5分钟',
+                                difficulty: '困难',
+                                calorie: '400千卡',
+                            },
+                            {
+                                title: '黑豆冬耗生地汤',
+                                time: '5分钟',
+                                difficulty: '困难',
+                                calorie: '400千卡',
+                            },
+                            {
+                                title: '桂圆红枣黑米粥',
+                                time: '5分钟',
+                                difficulty: '困难',
+                                calorie: '400千卡',
+                            },
+                        ]
+                    },
+                    {
+                        specialTitle: '清凉过小暑',
+                        data: [
+                            {
+                                title: '椒香凉拌莴笋结',
+                                time: '5分钟',
+                                difficulty: '困难',
+                                calorie: '400千卡',
+                            },
+                            {
+                                title: '鸡丝凉面',
+                                time: '5分钟',
+                                difficulty: '困难',
+                                calorie: '400千卡',
+                            },
+                            {
+                                title: '椰汁西米露',
+                                time: '5分钟',
+                                difficulty: '困难',
+                                calorie: '400千卡',
+                            },
+                            {
+                                title: '枸杞甜米酒汤圆',
+                                time: '5分钟',
+                                difficulty: '困难',
+                                calorie: '400千卡',
+                            },
+                        ]
+                    },
+                ]
+            },
+            {
+                title: '好味超下饭',
+                foodMenu:
+                [
+                    {
+                        title: '台式卤肉饭',
+                        tool: '小烤箱',
+                        imgURL: ''
+                    },
+                    {
+                        title: '口水鸡',
+                        tool: '小烤箱',
+                        imgURL: ''
+                    },
+                    {
+                        title: '普宁豆酱小白鲳',
+                        tool: '小烤箱',
+                        imgURL: ''
+                    },
+                ],
+            },
+            {
+                title: '家常快手菜',
+                foodMenu:
+                [
+                    {
+                        title: '洋葱牛柳丝',
+                        tool: '小烤箱',
+                        imgURL: ''
+                    },
+                    {
+                        title: '日式炒牛蒡',
+                        tool: '小烤箱',
+                        imgURL: ''
+                    },
+                    {
+                        title: '顶花椒瓜拌贝丝',
+                        tool: '小烤箱',
+                        imgURL: ''
+                    },
+                ],
+            },
+            {
+                title: '健康主食',
+                foodMenu:
+                [
+                    {
+                        title: '台式卤肉饭',
+                        tool: '小烤箱',
+                        imgURL: ''
+                    },
+                    {
+                        title: '翡翠白菜饺子',
+                        tool: '小烤箱',
+                        imgURL: ''
+                    },
+                    {
+                        title: '黄麦饭',
+                        tool: '小烤箱',
+                        imgURL: ''
+                    },
+                ],
+            },
+        ]
+    },
+]
+
+exports.data = data
